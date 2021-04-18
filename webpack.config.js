5f292e53a3d03c705fb187530b43590ff54cf195
Sahil811 <sahilsiddiqui.dev@gmail.com>
2021-04-18 21:56:46 +0530

    master: firebase setup/public

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ const htmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
    entry: ['@babel/polyfill', './src/js/index.js'],
    output: {
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'public'),
       filename: 'js/bundle.js'
    },
    devServer: {
-       contentBase: './dist'
+       contentBase: './public'
    },
    plugins: [
        new htmlWebpackPlugin({
